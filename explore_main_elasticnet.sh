0fc052057f201aee4f6bb5774dc6393c065959cc
dol0828 <40868984+dol0828@users.noreply.github.com>
2019-10-29 18:32:39 +0100

    Update explore_main_elasticnet.sh

--- explore_main_elasticnet.sh
+++ explore_main_elasticnet.sh
@@ -1,4 +1,4 @@
-for alpha in 0.15 0.25 0.35 0.45 0.55 0.65 0.75 ; do
+for alpha in 0.45 0.55 0.65 0.75 0.85 ; do
   for l1_ratio in 0.25 0.5 0.75 ; do
     echo "============================================="
     echo "python main_elasticnet.py $alpha $l1_ratio"
