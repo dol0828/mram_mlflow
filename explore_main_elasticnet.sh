9c2853088fe6bfb9620b6dd89a1afd2e0a10b560
dol0828 <40868984+dol0828@users.noreply.github.com>
2019-10-29 14:53:49 +0100

    Add files via upload

---
+++ explore_main_elasticnet.sh
@@ -0,0 +1,8 @@
+for alpha in 0.15 0.25 0.35 0.45 0.55 0.65 0.75 ; do
+  for l1_ratio in 0.25 0.5 0.75 ; do
+    echo "============================================="
+    echo "python main_elasticnet.py $alpha $l1_ratio"
+    echo "============================================="
+    python main_elasticnet.py $alpha $l1_ratio
+  done
+done
